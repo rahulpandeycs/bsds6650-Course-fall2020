b9e68e94f09098567773665c6f1c6bf412e66a66
rahulpandeycs <rahul.office80@gmail.com>
2020-10-06 15:52:31 -0400

    added default jars and support for config file read via command line

--- Assignment 1/bsds-cs6650-hw1-clientPart2/src/main/java/RestApiClientMain.java
+++ Assignment 1/bsds-cs6650-hw1-clientPart2/src/main/java/RestApiClientMain.java
@@ -15,19 +15,35 @@ public class RestApiClientMain {
 
   public static void main(String[] args) {
 
-    try (InputStream input = RestApiClientMain.class.getClassLoader().getResourceAsStream("config.properties")) {
+    try {
+      java.io.File propertiesFile = null;
+      InputStream input = null;
+      if (args.length > 1 && args[0].equals("-f")) {
+        propertiesFile = new java.io.File(args[1]);
+        System.out.println("Reading file: " + args[1]);
+        input = new java.io.FileInputStream(propertiesFile);
+      } else {
+        System.out.println("Reading default config.properties");
+        input = RestApiClientMain.class.getClassLoader().getResourceAsStream("config.properties");
+      }
       Properties prop = new Properties();
       if (input == null) {
+        System.out.println("Sorry, unable to find config.properties");
         logger.error("Sorry, unable to find config.properties");
         return;
       }
       //load a properties file from class path, inside static method
       prop.load(input);
       ConfigParameters parameters = new ConfigParameters(prop);
+      if (!(parameters.getNumLifts() >= 5 && parameters.getNumLifts() <= 60)){
+        System.out.println("Execution abort! Number of Lifts should be in range 5-60");
+        return;
+      }
       ThreadManager threadManager = new ThreadManager(parameters);
       Thread mainThread = new Thread(threadManager);
       mainThread.start();
     } catch (IOException ex) {
+      System.out.println("The config.properties not present");
       logger.error("The config.properties not present");
     }
   }
